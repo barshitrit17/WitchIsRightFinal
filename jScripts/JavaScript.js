21f8f636e293426c8e92d2a6fd55e28e8c76bac7
barshitrit17 <barshitrit17@gmail.com>
2022-02-07 02:17:32 +0200

    FINAL

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,24 @@
+﻿(function ()
+{
+    // how to play click
+    var howToPlayElement = window.document.getElementsByClassName("howToPlay")[0];
+    howToPlayElement.onclick = function () {
+        window.document.getElementsByClassName("howToPlayImage")[0].style.display = "block";
+        window.document.getElementsByClassName("closeButton")[0].style.display = "block";
+    }
+
+    // about click
+    var aboutElement = window.document.getElementsByClassName("about")[0];
+    aboutElement.onclick = function () {
+        window.document.getElementsByClassName("aboutImage")[0].style.display = "block";
+        window.document.getElementsByClassName("closeButton")[0].style.display = "block";
+    }
+
+    // close click
+    var closeButtonElement = window.document.getElementsByClassName("closeButton")[0];
+    closeButtonElement.onclick = function () {
+        window.document.getElementsByClassName("howToPlayImage")[0].style.display = "none";
+        window.document.getElementsByClassName("aboutImage")[0].style.display = "none";
+        window.document.getElementsByClassName("closeButton")[0].style.display = "none";
+    }
+})();
